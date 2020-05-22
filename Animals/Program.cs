3eaee49dd0e336a80cd102771d8d86c955ae3a66
DizdarArmin <armin.dizdar@gmail.com>
2020-05-22 10:25:56 +0200

    Animals

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -10,6 +10,13 @@ namespace Animals
     {
         static void Main(string[] args)
         {
+            Animal cat = new Cat("Peter", "Whiskas");
+            Animal dog = new Dog("Gunnar", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+            Console.ReadLine();
         }
     }
 }
